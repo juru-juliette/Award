931f276f3e8d25c41568b8bdc7e11a140d615c7a
juru-juliette <kangabejuliette@gmail.com>
2019-10-25 14:06:39 +0200

    creating static

--- Awards/settings.py
+++ Awards/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'award.apps.AwardConfig',
-    'bootstrap3',
+    'bootstrap4',
 ]
 
 MIDDLEWARE = [
@@ -120,3 +120,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
