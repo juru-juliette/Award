e6a40aa44f3f7dc32c587fef83c24b87b5232979
juru-juliette <kangabejuliette@gmail.com>
2019-10-25 16:49:54 +0200

    first migration

---
+++ award/forms.py
@@ -0,0 +1,6 @@
+from django import forms
+from .models import Project
+class NewPostForm(forms.ModelForm):
+    class Meta:
+        model = Project
+        exclude = ['']
--- award/views.py
+++ award/views.py
@@ -1,8 +1,28 @@
 from django.shortcuts import render,redirect
 from django.http  import HttpResponse,HttpResponseRedirect
+from .models import Project
+from django.contrib.auth.decorators import login_required
+from .forms import NewPostForm
+from django.contrib.auth.models import User
 
 # Create your views here.
-# @login_required(login_url='/accounts/login/')
+@login_required(login_url='/accounts/login/')
 def home(request):
     title='Awards'
     return render(request,'AW/home.html',{'title':title })
+
+@login_required(login_url='/accounts/login/')
+def new_post(request):
+    current_user = request.user
+    if request.method == 'POST':
+        form = NewPostForm(request.POST, request.FILES)
+        if form.is_valid():
+            image = form.save(commit=False)
+            image.user = current_user
+            
+            image.save()
+        return redirect('home')
+
+    else:
+        form = NewPostForm()
+    return render(request, 'AW/post.html', {"form": form})
