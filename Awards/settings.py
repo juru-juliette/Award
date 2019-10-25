c8d787167aed2cc9425190de10f305dd0e9408b4
juru-juliette <kangabejuliette@gmail.com>
2019-10-25 16:39:08 +0200

    first migration

--- Awards/settings.py
+++ Awards/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'award.apps.AwardConfig',
     'bootstrap4',
+    'tinymce',
 ]
 
 MIDDLEWARE = [
--- award/models.py
+++ award/models.py
@@ -1,13 +1,13 @@
 from django.db import models
 from django.contrib.auth.models import User
 import datetime as dt 
+from tinymce.models import HTMLField
 # Create your models here.
 class Profile(models.Model):
     photo=models.ImageField(upload_to='pic/')
     bio=models.TextField()
     username=models.OneToOneField(User,on_delete=models.CASCADE,related_name='profile')
-    project=models.ForeignKey(Project, null=True)
-    email = models.EmailField(null=true)
+    email = models.EmailField(max_length=100)
     def save_profile(self):
         self.save()
     def delete_profile(self):
@@ -43,5 +43,3 @@ class Review(models.Model):
      content=HTMLField()
      profile=models.ForeignKey(Profile, null=True)
      project=models.ForeignKey(Project, null=True)
-
-
