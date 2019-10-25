f41913bc2778344fa673532f5f6c78da0e59e71e
juru-juliette <kangabejuliette@gmail.com>
2019-10-25 16:14:17 +0200

    creating models

--- award/models.py
+++ award/models.py
@@ -1,6 +1,42 @@
-# -*- coding: utf-8 -*-
-from __future__ import unicode_literals
-
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+class Profile(models.Model):
+    photo=models.ImageField(upload_to='pic/')
+    bio=models.TextField()
+    username=models.OneToOneField(User,on_delete=models.CASCADE,related_name='profile')
+    project=models.ForeignKey(Project, null=True)
+    email = models.EmailField(null=true)
+    def save_profile(self):
+        self.save()
+    def delete_profile(self):
+       self.delete()
+
+    def update_bio(self,bio):
+         self.bio=bio
+         self.save()
+    @classmethod
+    def search_by_username(cls,search_term):
+       users=cls.objects.filter(username__username__icontains=search_term)
+       return users
+class Project(models.Model):
+     title=models.CharField(max_length=100)
+     image=models.ImageField(upload_to = 'pic/')
+     description=HTMLField()
+     pub_date = models.DateTimeField(auto_now_add=True)
+     profile=models.ForeignKey(Profile, null=True)
+     user=models.ForeignKey(User,on_delete=models.CASCADE,null=True)
+    
+     def save_project(self):
+         self.save()
+
+     def delete_project(self):
+       self.delete()
+       
+     def update_description(self,des):
+         self.description=des
+         self.save() 
+
+
+
+
