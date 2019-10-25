eada33c855120fb6e118ddb67f5abb23674e1e46
juru-juliette <kangabejuliette@gmail.com>
2019-10-25 14:52:41 +0200

    creating form for signup

--- Awards/settings.py
+++ Awards/settings.py
@@ -78,7 +78,7 @@ WSGI_APPLICATION = 'Awards.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'Award',
+        'NAME': 'award',
         'USER': 'wecode',
     'PASSWORD':'juruyuly1',
     }
--- award/views.py
+++ award/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render,redirect
 from django.http  import HttpResponse,HttpResponseRedirect
+
 # Create your views here.
 # @login_required(login_url='/accounts/login/')
 def home(request):
