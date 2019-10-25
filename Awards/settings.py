e91167413cfe2bb474f08e6d6fd207951406f527
juru-juliette <kangabejuliette@gmail.com>
2019-10-25 14:31:09 +0200

    creating authentication

--- Awards/settings.py
+++ Awards/settings.py
@@ -77,8 +77,10 @@ WSGI_APPLICATION = 'Awards.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'Award',
+        'USER': 'wecode',
+    'PASSWORD':'juruyuly1',
     }
 }
 
